4fffb3ea18d78f49663bd0a299ebca5e65576d97
Sunrise9871 <pubgplayer9871@gmail.com>
2024-06-13 02:35:03 +0800

    AI

---
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LevelGeneration;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 10;
+        [SerializeField] private Transform firstDestination;
+        [SerializeField] private Transform feet;
+
+        private Queue<Transform> _paths;
+        private bool _isJumping;
+
+        private void Awake()
+        {
+            _paths = new Queue<Transform>();
+            LevelGenerator.OnPlatformSpawned += AddNewSpawnedPlatform;
+        } 
+        private void OnDisable() => LevelGenerator.OnPlatformSpawned -= AddNewSpawnedPlatform;
+
+        private void Start() => StartCoroutine(Jump(firstDestination));
+        private void AddNewSpawnedPlatform(Transform spawnedPlatform)
+        {
+            _paths.Enqueue(spawnedPlatform);
+        }
+
+        private void SetNewDestination()
+        {
+            while (_paths.TryDequeue(out var platform))
+            {
+                print(gameObject.transform.position.y - platform.position.y);
+                if (platform.position.y - gameObject.transform.position.y < 8) continue;
+                StartCoroutine(Jump(platform));
+                break;
+            }
+        }
+
+        private IEnumerator Jump(Transform currentPath)
+        {
+            while (Vector3.Distance(feet.position, currentPath.position) > 0.1)
+            {
+                print(Vector3.Distance(feet.position, currentPath.position));
+                var direction = currentPath.position - feet.position;
+                gameObject.transform.Translate(direction.normalized * (speed * Time.deltaTime));
+                yield return null;
+            }
+
+            SetNewDestination();
+        }
+    }
+}
--- Assets/Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -9,7 +10,7 @@ namespace LevelGeneration
         [SerializeField] private List<GameObject> platforms;
         [SerializeField] private Transform player;
         [SerializeField] private Transform redZone;
-        [SerializeField] private Transform lastPlatformPosition;
+        [SerializeField] private Transform lastPlatform;
         [SerializeField] private float checkLevelRepeatTime;
         
         [Tooltip("Насколько ниже должна быть платформа по сравнению с red zone")]
@@ -19,7 +20,9 @@ namespace LevelGeneration
         private Queue<Transform> _activePlatformsQueue;
         private const float MIN_DISTANCE_TO_SPAWN_PLATFORM = 20f;
         
-        private void Start()
+        public static Action<Transform> OnPlatformSpawned;
+        
+        private void Awake()
         {
             _activePlatformsQueue = new Queue<Transform>();
             InvokeRepeating(nameof(GenerateLevel), 0f, checkLevelRepeatTime);
@@ -31,15 +34,14 @@ namespace LevelGeneration
         private void GenerateLevel()
         {
             //Если red zone выше платформы, то удалить платформу
-            while (_activePlatformsQueue.TryPeek(out var highestPlatform))
-                if (highestPlatform.position.y < redZone.transform.position.y - redZoneDifference)
+            while (_activePlatformsQueue.TryPeek(out var lowestPlatform))
+                if (lowestPlatform.position.y < redZone.transform.position.y - redZoneDifference)
                     DestroyLowestPlatform();
                 else break;
 
             //Если игрок приближается к самой высокой платформе, то создать еще платформу
-            if (!lastPlatformPosition) return;
-            while (Vector3.Distance(player.position, lastPlatformPosition.position) <
-                   MIN_DISTANCE_TO_SPAWN_PLATFORM)
+            if (!lastPlatform) return;
+            while (lastPlatform.position.y < player.position.y + MIN_DISTANCE_TO_SPAWN_PLATFORM)
                 SpawnPlatform();
         }
 
@@ -55,14 +57,16 @@ namespace LevelGeneration
         {
             //Вычисление новой позиции для платформы
             var newPosition = new Vector3(
-                lastPlatformPosition.position.x + Random.Range(minX, maxX),
-                lastPlatformPosition.position.y + Random.Range(minY, maxY),
-                lastPlatformPosition.position.z);
+                Random.Range(minX, maxX),
+                lastPlatform.position.y + Random.Range(minY, maxY),
+                lastPlatform.position.z);
 
             //Выбор и создание случайного префаба платформы
-            lastPlatformPosition = Instantiate(platforms[Random.Range(0, platforms.Count)], newPosition,
+            lastPlatform = Instantiate(platforms[Random.Range(0, platforms.Count)], newPosition,
                 Quaternion.identity, gameObject.transform).transform;
-            _activePlatformsQueue.Enqueue(lastPlatformPosition);
+            _activePlatformsQueue.Enqueue(lastPlatform);
+            
+            OnPlatformSpawned?.Invoke(lastPlatform);
         }
     }
 }
