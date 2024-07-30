b6ef1d5f3006710b07bc41aa1e2e8138f934792e
Sunrise9871 <pubgplayer9871@gmail.com>
2024-07-30 22:35:54 +0800

    Simple background script and images added

---
+++ Assets/Scripts/GameLogic/Level/Background.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace GameLogic.Level
+{
+    public class Background : MonoBehaviour
+    {
+        private SpriteRenderer[] _spriteRenderers;
+        private UnityEngine.Camera _camera;
+
+        private void Awake()
+        {
+            _camera = UnityEngine.Camera.main;
+            _spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+            
+            ResizeSpriteToScreen();
+        }
+
+        private void ResizeSpriteToScreen()
+        {
+            foreach (var spriteRenderer in _spriteRenderers)
+            {
+                spriteRenderer.transform.localScale = Vector3.one;
+                
+                var width = spriteRenderer.sprite.bounds.size.x;
+                var height = spriteRenderer.sprite.bounds.size.y;
+                
+                var worldScreenHeight = _camera.orthographicSize * 2f;
+                var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+                
+                var xScale = worldScreenWidth / width;
+                var yScale = worldScreenHeight / height;
+                var newScale = new Vector3(xScale, yScale, 1f);
+
+                spriteRenderer.transform.localScale = newScale;
+            }
+        }
+    }
+}
