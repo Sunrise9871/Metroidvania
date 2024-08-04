8a90478f59bc37ecdff46a9776c2fe20f8be10e8
Sunrise9871 <pubgplayer9871@gmail.com>
2024-08-04 22:23:25 +0800

    Sounds and music added

---
+++ Assets/Scripts/GameLogic/Level/Music/BackgroundMusic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace GameLogic.Level.Music
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class BackgroundMusic : MonoBehaviour
+    {
+        [Tooltip("Список фоновой музыки")]
+        [SerializeField] private List<AudioClip> musicList;
+
+        private AudioSource _audioSource;
+        private int _playingClipIndex;
+        
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+
+            _playingClipIndex = Random.Range(0, musicList.Count);  
+            PlayNextClip();
+            
+            StartCoroutine(WaitForEndOfClip());
+        }
+
+        private void PlayNextClip()
+        {
+            _playingClipIndex = (_playingClipIndex + 1) % musicList.Count;
+            var clip = musicList[_playingClipIndex];
+            _audioSource.PlayOneShot(clip);
+
+            StartCoroutine(WaitForEndOfClip());
+        }
+
+        private IEnumerator WaitForEndOfClip()
+        {
+            yield return new WaitForSeconds(musicList[_playingClipIndex].length);
+
+            PlayNextClip();
+        }
+    }
+}
---
+++ Assets/Scripts/Shooting/BulletSound.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Shooting
+{
+    [RequireComponent(typeof(Bullet))]
+    [RequireComponent(typeof(AudioSource))]
+    public class BulletSound : MonoBehaviour
+    {
+        [Tooltip("Звук создания projectile")]
+        [SerializeField] private AudioClip creationSound;
+
+        [Tooltip("Звук взрыва projectile")]
+        [SerializeField] private AudioClip explosionSound;
+
+        private AudioSource _audioSource;
+        private Bullet _bullet;
+
+        private void Awake()
+        {
+            _bullet = GetComponent<Bullet>();
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        private void OnEnable()
+        {
+            _bullet.BulletExploded += OnExploded;
+            
+            _audioSource.PlayOneShot(creationSound);
+        } 
+        
+        private void OnDisable() => _bullet.BulletExploded -= OnExploded;
+
+        private void OnExploded() => _audioSource.PlayOneShot(explosionSound);
+    }
+}
